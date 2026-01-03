ed7f1943b82c45596f6031310121997336c715b6
Suraj Mandal <surajmandalcell@gmail.com>
2026-01-02 20:11:42 -0500

    Initial Commit

---
+++ powertoys/powertoysApp.swift
@@ -0,0 +1,17 @@
+//
+//  powertoysApp.swift
+//  powertoys
+//
+//  Created by Suraj Mandal on 2026-01-02.
+//
+
+import SwiftUI
+
+@main
+struct powertoysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
